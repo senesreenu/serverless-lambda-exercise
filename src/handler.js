908c22058c97badd3e276a834c4be6f7c8f2e7d9
sene <senesreenu@gmail.com>
2021-12-18 22:37:58 +0530

    First

---
+++ src/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+module.exports.s3TriggerToLambda = async (event) => {
+  const today = new Date();
+  console.log(today);
+  var params = {
+      Bucket : "testbucketfromlambda666",
+         Key : "test3.txt",
+        Body : today.toString()
+  };
+  var data = await s3.putObject(params).promise();
+  return data;
+};
