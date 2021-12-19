3d642a53d79625caa67c2ccec306a4c117765280
sene <senesreenu@gmail.com>
2021-12-19 22:35:44 +0530

    4 commit

--- src/s3TriggerToLambda/index.js
+++ src/s3TriggerToLambda/index.js
@@ -3,7 +3,7 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-module.exports.handler = async (event) => {
+exports.handler = async (event) => {
   const today = new Date();
   console.log(today);
   var params = {
